cca912d0325a24c20c0a2be3ca745639908c20c1
Cleiton <139487277+cleitoncfs@users.noreply.github.com>
2024-08-19 21:49:43 +0100

    Resolução de conflitos git

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMobile = document.querySelector('.nav-mobile');
@@ -29,35 +28,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-=======
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMobile = document.querySelector('.nav-mobile');
-
-    menuToggle.addEventListener('click', function() {
-        navMobile.classList.toggle('active');
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const backToTopButton = document.querySelector('.back-to-top');
-
-    // Mostrar/Esconder o botão de acordo com o scroll
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 300) {
-            backToTopButton.classList.add('visible');
-        } else {
-            backToTopButton.classList.remove('visible');
-        }
-    });
-
-    // Rolagem suave ao topo
-    backToTopButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
-});
->>>>>>> 08eed79a6517bc64c3de4d432019fca295566c0d
