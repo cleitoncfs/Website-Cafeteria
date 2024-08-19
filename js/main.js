bbaecfc0172341dce3dd91b83fb583137083ab04
Cleiton <139487277+cleitoncfs@users.noreply.github.com>
2024-08-19 21:24:27 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMobile = document.querySelector('.nav-mobile');
+
+    menuToggle.addEventListener('click', function() {
+        navMobile.classList.toggle('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.querySelector('.back-to-top');
+
+    // Mostrar/Esconder o botão de acordo com o scroll
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopButton.classList.add('visible');
+        } else {
+            backToTopButton.classList.remove('visible');
+        }
+    });
+
+    // Rolagem suave ao topo
+    backToTopButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
